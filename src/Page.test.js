e543c28b535119a0a202ed4aff38ca1098c9b2b1
David Galvez <davidgalvezgomez01@gmail.com>
2021-10-16 19:11:41 +0200

    Added the test for AddList

--- src/Page.test.js
+++ src/Page.test.js
@@ -1,7 +1,7 @@
 import Page from "../build/Page.js";
 
 describe("Given a Page component", () => {
-  describe("When it recives a main container, a 'page' class and 'div' tag", () => {
+  describe("When it recives a div container, a 'page' class and 'div' tag", () => {
     test("Then it should render main element with class 'main' inside the div container", () => {
       const container = document.createElement("div");
       const className = "page";
