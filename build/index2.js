90da370c7660d7e15287e6f28ac8ed35fb437fdd
David Galvez <davidgalvezgomez01@gmail.com>
2021-10-16 15:57:05 +0200

    Created the class Page

--- build/index2.js
+++ build/index2.js
@@ -1,4 +1,4 @@
 import Page from "./Page.js";
 
 const fatherPage = document.querySelector(".page-container");
-const mainPage = new Page(fatherPage, "page", "div", "Pokedex");
+const mainPage = new Page(fatherPage, "page", "div", "My Pokemon Team");
