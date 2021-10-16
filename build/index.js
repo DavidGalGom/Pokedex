a4696d41df89dc752986355eae65777550ec79a8
David Galvez <davidgalvezgomez01@gmail.com>
2021-10-16 17:48:58 +0200

    Added all cards at main page

--- build/index.js
+++ build/index.js
@@ -14,3 +14,93 @@ const newPokemon = new AddList(
   "001",
   "Plant and Poison"
 );
+const newPokemon2 = new AddList(
+  fatherList,
+  "pokemon-card",
+  "li",
+  "Pokedex",
+  "Ivisaur",
+  "",
+  "002",
+  "Plant and Poison"
+);
+const newPokemon3 = new AddList(
+  fatherList,
+  "pokemon-card",
+  "li",
+  "Pokedex",
+  "Venusaur",
+  "",
+  "003",
+  "Plant and Poison"
+);
+const newPokemon4 = new AddList(
+  fatherList,
+  "pokemon-card",
+  "li",
+  "Pokedex",
+  "Charmander",
+  "",
+  "004",
+  "Fire"
+);
+const newPokemon5 = new AddList(
+  fatherList,
+  "pokemon-card",
+  "li",
+  "Pokedex",
+  "Charmaleon",
+  "",
+  "005",
+  "Fire"
+);
+const newPokemon6 = new AddList(
+  fatherList,
+  "pokemon-card",
+  "li",
+  "Pokedex",
+  "Charizard",
+  "",
+  "006",
+  "Fire"
+);
+const newPokemon7 = new AddList(
+  fatherList,
+  "pokemon-card",
+  "li",
+  "Pokedex",
+  "Squirtle",
+  "",
+  "007",
+  "Water"
+);
+const newPokemon8 = new AddList(
+  fatherList,
+  "pokemon-card",
+  "li",
+  "Pokedex",
+  "Wartortle",
+  "",
+  "008",
+  "Water"
+);
+const newPokemon9 = new AddList(
+  fatherList,
+  "pokemon-card",
+  "li",
+  "Pokedex",
+  "Blastoise",
+  "",
+  "009",
+  "Water"
+);
+const newPokemon10 = new AddList(
+  fatherList,
+  "pokemon-card",
+  "li",
+  "Pokedex",
+  "Caterpie",
+  "",
+  "010",
+  "Bug"
+);
